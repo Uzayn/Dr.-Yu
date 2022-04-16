4a274cb75a5059e1e76ba8dcd04dbef2115bcef2
Uzayn <koleoshohuzain@gmail.com>
2022-04-16 04:39:01 +0100

    First commit

---
+++ Dice game/index.js
@@ -0,0 +1,24 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage1 = "images/Dice" + randomNumber1 + ".jpg";
+
+document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage1 );
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage2 = "images/Dice" + randomNumber2 + ".jpg";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins!"
+}
+
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 wins!"
+}
+
+else {
+    document.querySelector("h1").innerHTML = "It's a draw!"
+}
