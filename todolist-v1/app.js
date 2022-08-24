da1ac57c95c509cc8108fbdfaec3f6bcf68abc6d
Koleosho Huzain <95490624+Uzayn@users.noreply.github.com>
2022-08-24 13:20:47 +0100

    update todo app

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -2,15 +2,42 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set('view engine', 'ejs')
 
 app.get("/", function(req, res) {
     let today = new Date();
+    let currentDay = today.getDay()
+    let day = ""
 
-    if (today.getDay() === 4) {
-        res.send("Yay it's the weekend")
-    } else {
-        res.send("Boo i have to work")
+    switch (currentDay) {
+        case 0:
+            day = "Sunday"
+            break;
+        case 1:
+            day = "Monday"
+            break;
+        case 2:
+            day = "Tuesday"
+            break;
+        case 3:
+            day = "Wednesday"
+            break;
+        case 4:
+            day = "Thursday"
+            break;
+        case 5:
+            day = "Friday"
+            break;
+        case 6:
+            day = "Saturday"
+            break;
+
+        default:
+            console.log("The value of the current Date is " + currentDay);
+            break;
     }
+
+    res.render("list", { kindOfDay: day })
 });
 
 app.listen(3000, function() {
