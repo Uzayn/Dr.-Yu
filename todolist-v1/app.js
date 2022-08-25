0574a45a2f9438182c39a994053f33c0ee5fa354
Koleosho Huzain <95490624+Uzayn@users.noreply.github.com>
2022-08-25 20:17:18 +0100

    to do updated

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,44 +1,33 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const app = express();
-app.set('view engine', 'ejs')
 
+
+const app = express();
+let items = [];
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static("public"))
 app.get("/", function(req, res) {
     let today = new Date();
-    let currentDay = today.getDay()
-    let day = ""
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday"
-            break;
-        case 1:
-            day = "Monday"
-            break;
-        case 2:
-            day = "Tuesday"
-            break;
-        case 3:
-            day = "Wednesday"
-            break;
-        case 4:
-            day = "Thursday"
-            break;
-        case 5:
-            day = "Friday"
-            break;
-        case 6:
-            day = "Saturday"
-            break;
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    };
 
-        default:
-            console.log("The value of the current Date is " + currentDay);
-            break;
-    }
+    let day = today.toLocaleDateString("en-US", options)
 
-    res.render("list", { kindOfDay: day })
+    res.render("list", { kindOfDay: day, newListItems: items })
 });
+
+app.post("/", function(req, res) {
+    item = req.body.newItem
+    items.push(item)
+    res.redirect("/")
+})
 
 app.listen(3000, function() {
     console.log("Server is now running");
